28215de3c8a9183f73a70e598d4f6bdb6bc39ec5
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2024-03-15 15:06:16 -0300

    Questão 5

---
+++ Atividades/Atividade5N/caso.py
@@ -0,0 +1,12 @@
+def calcular_delta(a, b, c):
+    delta = b**2 - 4*a*c
+    return delta
+
+a=2
+b=5
+c=-3
+
+delta = calcular_delta(a,b,c)
+print("O delta da equação é:", delta)
+
+#Não lembro como fazer delta. Então, sem chance de corrigir
