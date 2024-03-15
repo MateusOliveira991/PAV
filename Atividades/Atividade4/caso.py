60ec5bf5be5b540ec3fe67b47904b5c6c57bed4b
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2024-03-15 15:01:07 -0300

    questão 4

--- Atividades/Atividade4/caso.py
+++ Atividades/Atividade4/caso.py
@@ -1,4 +1,22 @@
+#Crie uma função que receba dois valores e informa se o primeiro valor é divisível pelo segundo ou não.
 
+def divisivel( valor1, valor2):
+    if valor2 == 0:
+        return "Denominator cannot be zero."
+
+    
+    if valor1 % valor2== 0:
+        return f"O {valor1} é divisível por {valor2}."
+    else: return f"O {valor1} NÃO é divisível por {valor2}."
+
+    #f dentro de return é para formatar a string, para que os valores das variáveis sejam impressos na string. ({valor1}).
+
+valor1 = float(input("Digite um número:"))
+valor2= float(input("Digite outro número:"))
+resultado = divisivel(valor1 , valor2)
+print(resultado)
+
+#como tirar o zero depois da virgula?
 
 
 
