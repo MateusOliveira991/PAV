b4d51c05e371ccef846d01860010417e6ad8d0b6
MateusOliveira991 <mateusaugusto9.91@gmail.com>
2024-03-25 18:24:33 -0300

    Iniciei a 8 mas tá dando erro

--- Atividades/Atividade8 Fazer/caso.py
+++ Atividades/Atividade8 Fazer/caso.py
@@ -1,1 +1,13 @@
 #8) Faça uma função que receba como parâmetro o raio de uma esfera, calcule e mostre no programa principal o seu volume: v = 4/3 * R3.
+
+def calcular_volume_esfera(raio):
+    volume = (4/3) * 3.14159 * (raio ** 3)
+    return 
+
+raio = float(input("Digite o raio da esfera: "))
+volume = calcular_volume_esfera(raio)
+
+print(f"O volume da esfera com raio {raio} é: {volume}")
+
+calcular_volume_esfera(raio)
+
